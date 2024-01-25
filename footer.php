581db0fa0de0de7382c49764f764db4208871e49
Meet Rathod <meetrn7890@gmail.com>
2024-01-25 23:45:47 +0530

    new file added

---
+++ footer.php
@@ -0,0 +1,88 @@
+
+    <!--extranal css file-->
+    <link href="font/fontawesome.min.css" rel="stylesheet">
+	<link href="footer.css" rel="stylesheet">
+	<link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    
+<section class="deneb_cta">
+	<div class="container mar">
+		<div class="cta_wrapper">
+			<div class="row align-items-center">
+				<div class="col-lg-7">
+					<div class="cta_content">
+						<h3>Registration as a vendor </h3>
+						<p>We help vendor to grow up his business. We provide the pletform for a vendor for grow his business.</p>
+					</div>
+				</div>
+				<div class="col-lg-5">
+					<div class="button_box">
+						<a href="vendor/vendor_reg3.php" class="btn btn-dark">Registration</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<footer class="deneb_footer">
+	<div class="widget_wrapper" >
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-12 pb-0">
+					<div class="widget widegt_about">
+						<div class="widget_title">
+						<div class="container mb-2 p-0 justify-content-center ">
+						<a class="navbar-brand" href="index.php"><span class="text-warning">VENDOR</span> <span class="text-light">PARTNERS</span>
+ 						 </a>
+						</div>
+						<p> The client can easilly find his choise vendors.</p>
+            <p class="mb-5 fw-bold">We are provide vendor to customer, but we have never lost sight of our core values. We believe in collaboration,
+				                      innovation, and customer satisfaction. We are always looking for new ways to improve our vendor.</p>
+						<ul class="social">
+							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12 ms-auto ps-5">
+					<div class="widget widget_contact">
+						<div class="widget_title">
+							<h4>Contact Us</h4>
+						</div>
+						<div class="contact_info">
+							<div class="single_info">
+								<div class="icon">
+									<i class="fas fa-phone-alt"></i>
+								</div>
+								<div class="info">
+									<p><a href="tel:+919246147999">+91 6355008608</a></p>
+									
+								</div>
+							</div>
+							<div class="single_info">
+								<div class="icon">
+									<i class="fas fa-envelope"></i>
+								</div>
+								<div class="info">
+									<p><a href="mailto:info@deneb.com">vendorpartners@gmail.com</a></p>
+								</div>
+							</div>
+							<div class="single_info">
+								<div class="icon">
+									<i class="fas fa-map-marker-alt"></i>
+								</div>
+								<div class="info">
+									<p>SGJ College, Mandvi<span>Kachchh</span></p>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			
+		</div>
+	</div>
---
+++ header.php
@@ -0,0 +1,50 @@
+<!--navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
+  <div class="container">
+    <a class="navbar-brand" href="index.php"><span class="text-warning">VENDOR</span> PARTNERS
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about_us.php">About us</a>
+        </li>
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" >
+            Vendor
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Decorators</a></li>
+            <li><a class="dropdown-item" href="#">photographers</a></li>
+            <li><a class="dropdown-item" href="#">vediographers</a></li>
+            <li><a class="dropdown-item" href="#">cake</a></li>
+            <li><a class="dropdown-item" href="#">flowers</a></li>
+            <li><a class="dropdown-item" href="#">wedding wear</a></li>
+            <li><a class="dropdown-item" href="#">jewellery</a></li>
+            <li><a class="dropdown-item" href="#">invitation cards</a></li>
+            <li><a class="dropdown-item" href="#">sound</a></li>
+            <li><a class="dropdown-item" href="#">planners</a></li>
+            <li><a class="dropdown-item" href="#">makeup artist</a></li>
+            <li><a class="dropdown-item" href="#">venues</a></li>
+            <li><a class="dropdown-item" href="#">gifts</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Log in
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="./client/login/reg.php">Login as client</a></li>
+            <li><a class="dropdown-item" href="#">Login as vendor</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
