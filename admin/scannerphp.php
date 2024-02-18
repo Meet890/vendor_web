de6cd2bc2e07f766c9df338aee607d265f75e8fa
Meet Rathod <meetrn7890@gmail.com>
2024-02-18 23:16:05 +0530

    footer phone icon

--- admin/scannerphp.php
+++ admin/scannerphp.php
@@ -2,10 +2,7 @@
     // Initialize the session
 session_start();
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["v_username"]) &&( $_SESSION["loggedin"] === true)){
-    header("location: vendor/");
-    exit;
-}
+
 // Include config file
 require_once "config.php";
 $idErr=$tra_id=$username='';
