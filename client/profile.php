862fd3220bf834a67d2c655848ee5b7affb5a5f0
Meet Rathod <meetrn7890@gmail.com>
2024-03-10 16:39:28 +0530

    sdafas

--- client/profile.php
+++ client/profile.php
@@ -51,7 +51,7 @@ else if(isset($_SESSION["username"])){
 
 
 
-    $result = mysqli_query($conn, "SELECT v_photo FROM vendor WHERE v_id = '$id' ");
+    $result = mysqli_query($conn, "SELECT v_photo FROM vendor WHERE v_id = $id ");
 
     while($row = $result->fetch_assoc()) {
         if($row['v_photo']==""){
@@ -155,19 +155,15 @@ if ($result->num_rows > 0) {
         
          foreach ($rows as $row) : ?>
                 <img src="../admin/vendor/img/<?php echo $row['g_photo']; ?>" class="col-4" onclick="openModal('../admin/vendor/img/<?php echo $row['g_photo']; ?>')" alt="Gallery Image 2">
-                <div id="myModal" class="modal">
-            <span class="close" onclick="closeModal()">&times;</span>
-            <img class="modal-content" id="modalImg ">
-        </div>
-
+                
             <?php endforeach; ?>   
             </div>
           
 
-        <!-- <div id="myModal" class="modal">
+        <div id="myModal" class="modal">
             <span class="close" onclick="closeModal()">&times;</span>
             <img class="modal-content" id="modalImg ">
-        </div> -->
+        </div>
 
 
         <!-- rateinggg -->
